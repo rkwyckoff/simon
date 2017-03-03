c022c1e815250b32753e35b3cddafb272c91b76f
Kurt Wyckoff <kurtitservices@gmail.com>
2017-03-03 14:04:56 -0500

    built boxes in html, some styling, began classes

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+import $ from "jquery";
+
+
+class Button {
+  constructor (options) {
+    options = options || {};
+    this.id = options.id;
+    // this.color = options.color;
+  // ??    this.completed = options.completed;
+  }
+
+  brightenColor () {
+    // grab the element for this Button
+    // make it get brighten
+    // set a timeout to make it go back to nornal in a half second
+  }
+
+}
+
+/*
+
+## Pattern
+* steps
+* guesses / playerSteps
+
+when button is clicked, we need to add a player's choice
+to guesses
+
+*/
